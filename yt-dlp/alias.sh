865c850722e836f429a4eb77f52824b0af8bf4f4
awsp <anthonyspwu@gmail.com>
2022-04-03 16:42:05 -0700

    fix ytdlp

--- yt-dlp/alias.sh
+++ yt-dlp/alias.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo 'alias yt="./yt-dlp -f \"bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best\" "' >> ~/.zshrc
+echo 'alias yt="yt-dlp -f \"bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best\" "' >> ~/.zshrc
