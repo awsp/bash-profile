640926d92844d1c0b7740b37ddddc90251b90fbf
Anthony Wu <anthony.wu@choicehotels.com>
2024-12-11 21:41:48 -0700

    update sketchybar

--- dotfiles/sketchybar/items/spaces.sh
+++ dotfiles/sketchybar/items/spaces.sh
@@ -18,34 +18,52 @@ function getLabelSize() {
 }
 
 # Beginning of the loop
-counter=0
-for sid in $(aerospace list-workspaces --all); do
-
-  if [ $counter -eq 5 ]; then
-    break
-  fi
-
+for sid in $(aerospace list-workspaces --monitor 1); do
   sketchybar --add item space.$sid left \
       --subscribe space.$sid aerospace_workspace_change \
       --set space.$sid \
+      drawing=off \
       background.color=0x44ffffff \
       background.corner_radius=5 \
       background.height=20 \
-      background.drawing=off \
-      icon=$sid. \
+      background.drawing=on \
+      icon="$sid" \
       icon.highlight_color=0xfffff68f \
       label.highlight_color=0xfffff68f \
-      icon.padding_left=8 \
+      icon.padding_left=4 \
       icon.padding_right=2 \
-      label.padding_right=8 \
-      label="$(getLabel $sid)" \
+      label.padding_left=4 \
+      label.padding_right=18 \
+      label.font="sketchybar-app-font:Regular:16.0" \
       label.font.size="$(getLabelSize)" \
       click_script="aerospace workspace $sid" \
       script="$CONFIG_DIR/plugins/aerospace.sh $sid"
-
-  ((counter++))
 done
 # Ending of the loop
+
+# Load Icons on startup
+for sid in $(aerospace list-workspaces --monitor 1 --empty no); do
+  apps=$(aerospace list-windows --workspace "$sid" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+
+  sketchybar --set space.$sid drawing=on
+
+  icon_strip=" "
+  if [ "${apps}" != "" ]; then
+    while read -r app; do
+      icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
+    done <<<"${apps}"
+  else
+    icon_strip=""
+  fi
+  sketchybar --set space.$sid label="$icon_strip"
+done
+
+sketchybar --add item space_separator left \
+  --set space_separator icon="›" \
+  icon.padding_left=4 \
+  label.drawing=off \
+  script="$PLUGIN_DIR/space_windows.sh" \
+  --subscribe space_separator aerospace_workspace_change
 
 spaces=(
   background.color=$BACKGROUND_1
